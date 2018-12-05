99a71666ac6f6bcfacdd7f7f738c75dcd5fd9bf9
alifu <alif@docotel.co.id>
2018-12-05 13:43:42 +0800

    Variables

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+// Jenis variable berdasarkan jenis
+var variabelDinamis = "Ini bisa dirubah"          // deklarasi variable yg dapat diubah
+let variabelKonstan = "Ini tidak bisa dirubah"    // deklarasi variable yg tidak dapat diubah
+
+
