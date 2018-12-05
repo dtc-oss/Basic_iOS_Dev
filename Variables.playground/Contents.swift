fd5847a5e5e4c0f3d6551cfa708e270dc19f7518
alifu <alif@docotel.co.id>
2018-12-05 13:54:54 +0800

    String

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -1,7 +1,14 @@
 import UIKit
 
-// Jenis variable berdasarkan jenis
+// variable berdasarkan jenis
 var variabelDinamis = "Ini bisa dirubah"          // deklarasi variable yg dapat diubah
 let variabelKonstan = "Ini tidak bisa dirubah"    // deklarasi variable yg tidak dapat diubah
 
+// Deklarasi Variable dengan tipe data
 
+// String
+var string_1 = "Ini String"           // deklarasi variable yg tipe data berdasarkan isinya
+var string_2: String                  // deklarasi variable dengan menentukan tipe data nya
+var string_3: String?                 // sama dgn sebelumnya, namun menggunakan Optional variable (is Null)
+var string_4: String!                 // variable bersifat 'wrap' / non Optional (not Null)
+var string_5: String = "Ini String"   // varible dgn tipe data dan memiliki nilai default
