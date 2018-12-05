88603d020bf81c8a78d75da86fb2f510889cd821
alifu <alif@docotel.co.id>
2018-12-05 14:09:45 +0800

    Integer

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -13,7 +13,7 @@ var string_3: String!                 // variable bersifat 'wrap' / non Optional
 var string_4: String = "Ini String"   // varible dgn tipe data dan memiliki nilai default
 
 // Int = Tipe Data Integer
-var int_1 = "Ini Integer"           // deklarasi variable yg tipe data berdasarkan isinya
-var int_2: String?                  // Deklarasi variable Optional (is Null), ketika akan digunakan harus di 'wrap'
-var int_3: String!                  // variable bersifat 'wrap' / non Optional (not Null)
-var int_4: String = "Ini Integer"   // varible dgn tipe data dan memiliki nilai default
+var int_1 = 0           // deklarasi variable yg tipe data berdasarkan isinya
+var int_2: Int?         // Deklarasi variable Optional (is Null), ketika akan digunakan harus di 'wrap'
+var int_3: Int!         // variable bersifat 'wrap' / non Optional (not Null)
+var int_4: Int = 0      // varible dgn tipe data dan memiliki nilai default
