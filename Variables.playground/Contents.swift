10a6adcfad49fd8b07430e2eac055231856a032b
alifu <alif@docotel.co.id>
2018-12-26 13:13:51 +0800

    array, float, double, character, bool

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -12,8 +12,22 @@ var string_2: String?                 // Deklarasi variable Optional (is Null), 
 var string_3: String!                 // variable bersifat 'wrap' / non Optional (not Null)
 var string_4: String = "Ini String"   // varible dgn tipe data dan memiliki nilai default
 
+// cara deklarasi variable sama seperti di atas
+
 // Int = Tipe Data Integer
-var int_1 = 0           // deklarasi variable yg tipe data berdasarkan isinya
-var int_2: Int?         // Deklarasi variable Optional (is Null), ketika akan digunakan harus di 'wrap'
-var int_3: Int!         // variable bersifat 'wrap' / non Optional (not Null)
-var int_4: Int = 0      // varible dgn tipe data dan memiliki nilai default
+var int_1: Int = 0      // varible dgn tipe data dan memiliki nilai default
+
+// Int = Tipe Data Float
+var float_1: Float = 0.0      // varible dgn tipe data dan memiliki nilai default
+
+// Int = Tipe Data Double
+var double_1: Double = 0.0      // varible dgn tipe data dan memiliki nilai default
+
+// Int = Tipe Data Array dengan isi String
+var array_string: [String] = ["ini String"]      // varible dgn tipe data dan memiliki nilai default
+
+// Int = Tipe Data bool
+var bool_1: Bool = true     // varible dgn tipe data dan memiliki nilai default
+
+// Int = Tipe Data character
+var character_4: Character = "A"    // varible dgn tipe data dan memiliki nilai default
